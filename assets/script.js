ff6e3f9fae77836506f15b5e8642e9aef3dce647
Sarah <sarah.k.hopper@gmail.com>
2023-04-03 01:37:15 -0700

    refactor script.js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,42 @@
-const timerEl = document.getElementById("timer");
-const quizEl = document.getElementById("question");
-const choiceEl = document.getElementById("choices");
-
-const button = document.createElement("button");
-const para = document.createElement("p");
 
 const welcomeMsg = 
 "Welcome to the JavaScript Fundamentals Quiz. For each question, you'll have 10 seconds to choose the correct answer. When you're ready to start, click 'Start Quiz' below. Good luck!" ;
 
-// const questions = 
-// ["1. Commonly used data types do NOT include:", "2. String values must be enclosed with ___ when being assigned to variables", "3. The condition in an if/else statement is enclosed by:", "4. An array in JavaScript can be used to store:"];
-
-const q1 = "1. Commonly used data types do NOT include:";
-const q2 = "2. String values must be enclosed with ___ when being assigned to variables";
-const q3 = "3. The condition in an if/else statement is enclosed by:";
-const q4 = "4. An array in JavaScript can be used to store:";
-
-const q1choices = ["A. strings", "B. booleans", "C. alerts"];
-const q2choices = ["A. commas ", "B. curly brackets ", "C. quotes"];
-const q3choices = ["A. quotes ", "B. curly brackets ", "C. parentheses"];
-const q4choices = ["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"];
+const questions = [
+    { 
+        question: "Commonly used data types do NOT include:",
+        choices: ["A. strings", "B. booleans", "C. alerts"],
+        answer: "alerts"
+        
+    },
+    {
+        question: "String values must be enclosed with ___ when being assigned to variables",
+        choices: ["A. commas ", "B. curly brackets ", "C. quotes"],
+        answer: "quotes"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed by:",
+        choices: ["A. quotes ", "B. curly brackets ", "C. parentheses"],
+        answer: "parentheses"
+    },
+    {
+        question: "An array in JavaScript can be used to store:",
+        choices: ["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
+        answer: "all of the above"
+    }
+];
 
 const correct = "Correct";
 const incorrect = "Incorrect";
+
+const questionStart = 0;
+
+const timerEl = document.getElementById("timer");
+const quizEl = document.getElementById("question");
+const choiceEl = document.getElementById("choices");
+
+const newBtn = document.createElement("button");
+const newPara = document.createElement("p");
 
 // timer
 function timer() {
@@ -44,13 +58,51 @@ function timer() {
     }, 1000);
 };
 
-function displayQuestion() {
-    //display q1 and q1 choices after user clicks start btn
-    //display q2 and q2 choices after user answers q1 (clicks one of the btns)
-    //display q3 and q3 choices after user answers q2
-    //display q4 and q4 choices after user answers q4
-    //display quiz done msg and user's score after user answers q4, with option to enter initials and save score, button to go back to start and button to view scores
+// append button with answer choices text to choiceEl and assign id
+function answerBtn(text, id){
+    newBtn.textContent = text;
+    newBtn.setAttribute("id", id);
 };
+
+function displayQuestion(questionStart) {
+    const choices = questions[questionStart].choices;
+    const listItem = document.createElement("li");
+
+    quizEl.textContent = questions[questionStart].question;
+    
+    for (var i = 0; i < questions.length;) {
+        questions[i].textContent = questions.question;   
+        quizEl.appendChild(choiceEl);
+    };
+    
+    let i = 1;
+        choices.forEach(function(choice) {
+            choiceEl.appendChild(listItem); 
+                listItem.appendChild(answerBtn(i + ") " + choice + "liBtn"))
+
+            listItem.addEventListener("click", (ansEval));
+
+            i++;
+        });
+    };
+
+function ansEval(event) {
+    // listen for button click
+    // check if correct answer chosen
+    const ansBtn = event.target;
+
+    if (ansBtn.textContent == (questions[i].answer)) {
+        choiceEl.appendChild(newPara);
+        newPara.textContent = correct;
+    } else {
+        choiceEl.appendChild(newPara);
+        newPara.textContent = incorrect;
+    }
+
+    questionStart++; //advance to next question regardless of result
+}
+
+    //display quiz done msg and user's score after user answers q4, with option to enter initials and save score, button to go back to start and button to view scores
 
 // function displayQuestion() {
 
@@ -72,6 +124,17 @@ function displayQuestion() {
 //                  }
 
 timer();
+
+// function highScores() {
+    // const newH1 = document.createElement("h1");
+
+    //  quizEl.innerHTML = " "; 
+    // timerEl.textContent = " ";
+
+    // quizEl.appendChild(newH1("High Scores:"));
+// };
+
+// TODO: write fxn to calculate and record score
 
 //start button to start timer
 //append question and choices to pg
